d75b6fd1c47d01f3fdbc79f55e88e0c90180f3ba
Eugene <eugene.vintsiv@gmail.com>
2018-10-29 17:33:45 +0200

    Initial Commit

---
+++ EK-Lesson-2-classes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EK-Lesson-2-classes
+//
+//  Created by Eugene on 29.10.18.
+//  Copyright © 2018 Eugene. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
